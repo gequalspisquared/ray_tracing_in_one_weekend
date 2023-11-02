9f83a89d0f63d1966631a197da843f0df7e13d14
Nicholas Crane Laptop <nickcrusher30@gmail.com>
2023-11-01 20:19:51 -0600

    Implement anti-aliasing

--- src/camera.h
+++ src/camera.h
@@ -8,8 +8,9 @@
 
 class camera {
 public:
-    double aspect_ratio = 16.0 / 9.0;
-    int    image_width  = 100;
+    double aspect_ratio      = 16.0 / 9.0;
+    int    image_width       = 100;
+    int    samples_per_pixel = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -19,13 +20,12 @@ public:
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
             for (int i = 0; i < image_width; i++) {
-                vec3 pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                vec3 ray_direction = pixel_center - center;
-                ray r(center, ray_direction);
-
-
-                color pixel_color = ray_color(r, world);
-                write_color(std::cout, pixel_color);
+                color pixel_color(0, 0, 0);
+                for (int sample = 0; sample < samples_per_pixel; sample++) {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, world);
+                }
+                write_color(std::cout, pixel_color, samples_per_pixel);
             }
         }
 
@@ -61,6 +61,22 @@ private:
         // Calculate the location of the upper left pixel
         vec3 viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5*(pixel_delta_u + pixel_delta_v);
+    }
+
+    ray get_ray(int i, int j) const {
+        point3 pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+        point3 pixel_sample = pixel_center + pixel_sample_square();
+
+        point3 ray_origin = center;
+        vec3 ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 pixel_sample_square() const {
+        double px = -0.5 + random_double();
+        double py = -0.5 + random_double();
+        return (px * pixel_delta_u) + (py * pixel_delta_v);
     }
 
     color ray_color(const ray& r, const hittable& world) const {
